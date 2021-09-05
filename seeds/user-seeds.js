5ee08fe16583ce57ff647c9f53cda27d8f1b7f38
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-05 14:03:26 -0400

    Added the user-seeds.js that will seed the database with users to start

---
+++ seeds/user-seeds.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+
+const userData = [{
+    username: 'Xandromus',
+    password: '09876543'
+},
+{
+    username: 'Lernantino',
+    password: '98765432'
+},
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
