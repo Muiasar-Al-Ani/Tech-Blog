d09f7ae6af7d94e91a2d645a9d5258e104dc4915
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-05 18:56:19 -0400

    Added the user-routes which has a gitAll users, get user by id, create new user, login , logout, update user, and delete user

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,157 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbUserData = await User.findAll({
+      attributes: { exclude: ["[password"] },
+    });
+    res.json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      attributes: { exclude: ["password"] },
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ["id", "title", "content", "created_at"],
+        },
+
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "created_at"],
+          include: {
+            model: Post,
+            attributes: ["title"],
+          },
+        },
+        {
+          model: Post,
+          attributes: ["title"],
+        },
+      ],
+    });
+    if (!dbUserData) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
+    });
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that username!" });
+      return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const dbUserData = await User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!dbUserData[0]) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const dbUserData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!dbUserData) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(dbUserData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,7 +40,7 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
-router.get("/edit/:id", withAuth, (req, res) => {
+router.get("/edit/:id", withAuth, async (req, res) => {
   if (!req.session.loggedIn) {
     res.redirect("/login");
   } else {
