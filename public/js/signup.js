a18bcbbc9cfafa2e1a8586a8fea4dbc77786a817
Muiasar-Al-Ani <muiasar2012@gmail.com>
2021-09-06 09:24:39 -0400

    Added the signup.js that will handle the front end part of the sign up form

---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+const signupFormHandler = async event => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,7 +1,7 @@
 const { Post } = require('../models');
 
 const postData = [{
-        title: 'Why MVC is sp important',
+        title: 'Why MVC is so important',
         content: 'MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic',
         user_id: 1
     },
